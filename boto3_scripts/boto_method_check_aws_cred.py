30429860b67f0980d9016bc4fc21444521661271
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-09-11 11:09:47 +0530

    Create boto_method_check_aws_cred.py

---
+++ boto3_scripts/boto_method_check_aws_cred.py
@@ -0,0 +1,29 @@
+import boto3
+from botocore.exceptions import ProfileNotFound
+
+def read_aws_credentials(profile='default'):
+    try:
+      # Create a session using the specified profile
+      session = boto3.Session(profile_name=profile)
+        
+      # Get credentials from the session
+      credentials = session.get_credentials()
+        
+      # Credentials can be None if the profile is not set up properly
+      if credentials is None:
+        print(f"No credentials found on profile: {profile}")
+        return None, None
+        # Access key and secret key
+        access_key = credentials.access_key
+        secret_key = credentials.secret_key
+        return access_key, secret_key
+    except ProfileNotFound:
+      print(f"Profile '{profile}' not found.")
+      return None, None
+
+# Usage
+access_key, secret_key = read_aws_credentials()
+print("")
+print(f"Printing the available AWS access & secret key values..")
+print(f"AWS access_key: {access_key}")
+print(f"AWS secret_key: {secret_key}")
