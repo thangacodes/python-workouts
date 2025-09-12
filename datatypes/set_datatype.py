03e2fb4eb423083ad0dbd3cf2c6d7968d0ef83c6
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-09-12 10:56:27 +0530

    initial commit

---
+++ datatypes/set_datatype.py
@@ -0,0 +1,10 @@
+"""
+Sets in Python are unordered collections. This means:
+The order of items in a set is not guaranteed.
+Every time you print a set, the order of elements may appear different.
+Sets are also unique collections, so duplicate values are not allowed.
+"""
+
+orders = {"soap","shampoo","oil","comb","soap"}
+print(orders)
+print(type(orders))
